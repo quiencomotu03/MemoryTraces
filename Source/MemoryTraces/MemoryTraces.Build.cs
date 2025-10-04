a82dd315407d938b10cf6eb600080a11f16634c8
재미 🅵🆄🅽 게임 🅶🅰🅼🅴 <83849758+quiencomotu03@users.noreply.github.com>
2025-10-04 19:50:38 +0900

    first commit

---
+++ Source/MemoryTraces/MemoryTraces.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MemoryTraces : ModuleRules
+{
+	public MemoryTraces(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
